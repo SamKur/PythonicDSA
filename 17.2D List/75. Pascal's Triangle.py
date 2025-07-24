727435a9556bdaf16f7070222f396808f8b256e9
Susamay Kumbhakar <susamay.sk@gmail.com>
2025-07-24 09:24:47 +0530

    cmnt

--- 17.2D List/75. Pascal's Triangle.py
+++ 17.2D List/75. Pascal's Triangle.py
@@ -23,9 +23,9 @@ def generate(numRows):
     # Complexity - O(n2) and O(n2)
     triangle = []
     for i in range(numRows):
-        row = [1] * (i + 1)  # first and last element are always 1
+        row = [1] * (i + 1)     # setting all items as 1
         for j in range(1, i):  # fill inner elements
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j] # triangle[i - 1] -> prev row
         triangle.append(row)
     return triangle
 
