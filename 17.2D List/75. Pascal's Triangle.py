a8418d22fbb2371f2f74c6005f795caf6a117a2b
Susamay Kumbhakar <susamay.sk@gmail.com>
2025-07-24 08:31:14 +0530

    more way to solve

--- 17.2D List/75. Pascal's Triangle.py
+++ 17.2D List/75. Pascal's Triangle.py
@@ -1,4 +1,4 @@
-# from itertools import combinations
+# from math import comb
 def generate(numRows):
     """
     Function to generate the first numRows of Pascal's triangle.
@@ -16,22 +16,36 @@ def generate(numRows):
         OR, always starts with 1 and rest current element = previous row's current + prev
     """
     # TODO: Implement this function
-    if numRows == 0:
-        return []
 
-    triangle = [[1]]
+    # Complexity - O(n3) and O(n2)
+    # return [[comb(i, k) for k in range(i + 1)] for i in range(numRows)]
 
-    for i in range(1, numRows):
-        prev_row = triangle[-1]
-        current_row = [1]
-
-        for j in range(1, i):
-            current_row.append(prev_row[j - 1] + prev_row[j])
-
-        current_row.append(1)
-        triangle.append(current_row)
-
+    # Complexity - O(n2) and O(n2)
+    triangle = []
+    for i in range(numRows):
+        row = [1] * (i + 1)  # first and last element are always 1
+        for j in range(1, i):  # fill inner elements
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
     return triangle
+
+    # ALT - O(n2) and O(n2)
+    # if numRows == 0:
+    #     return []
+
+    # triangle = [[1]]
+
+    # for i in range(1, numRows):
+    #     prev_row = triangle[-1]
+    #     current_row = [1]
+
+    #     for j in range(1, i):
+    #         current_row.append(prev_row[j - 1] + prev_row[j])
+
+    #     current_row.append(1)
+    #     triangle.append(current_row)
+
+    # return triangle
 
 
 print(generate(5))
